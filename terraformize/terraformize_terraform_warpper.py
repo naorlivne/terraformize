a04ff3d2cb2bc31f6eb8a0ebb6b7650b0dea3440
naor <naorlivne@gmail.com>
2019-10-06 17:54:04 +0300

    templating hte terraform wrapper

---
+++ terraformize/terraformize_terraform_warpper.py
@@ -0,0 +1,15 @@
+from python_terraform import *
+
+class terraform:
+
+    def __init__(self):
+        # just a tempalte that still needs work
+        self.tf = Terraform()
+
+    def apply(self):
+        # just a tempalte that still needs work
+        self.tf.apply('/home/test123123', no_color=IsFlagged, refresh=False, var={'a':'b', 'c':'d'})
+
+    def destory(self):
+        # just a tempalte that still needs work
+        self.tf.destroy()
---
+++ test/test_terraformize_terraform_wrapper.py
@@ -0,0 +1,9 @@
+from unittest import TestCase, mock
+from terraformize.terraformize_terraform_warpper import *
+import os
+
+
+class BaseTests(TestCase):
+
+    def test_file_reader_read_file(self):
+        pass
