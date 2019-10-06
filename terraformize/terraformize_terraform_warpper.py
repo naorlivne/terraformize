43602de836a7152aa5afd4475ea64c4a13157187
naor <naorlivne@gmail.com>
2019-10-06 17:54:26 +0300

    templating hte terraform wrapper

--- terraformize/terraformize_terraform_warpper.py
+++ terraformize/terraformize_terraform_warpper.py
@@ -3,13 +3,13 @@ from python_terraform import *
 class terraform:
 
     def __init__(self):
-        # just a tempalte that still needs work
+        # just a template that still needs work
         self.tf = Terraform()
 
     def apply(self):
-        # just a tempalte that still needs work
+        # just a template that still needs work
         self.tf.apply('/home/test123123', no_color=IsFlagged, refresh=False, var={'a':'b', 'c':'d'})
 
     def destory(self):
-        # just a tempalte that still needs work
+        # just a template that still needs work
         self.tf.destroy()
