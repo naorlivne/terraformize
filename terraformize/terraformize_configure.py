d5a6de7c393112108f16633e8651cc7a79be0b6b
naorlivne <naorlivne@gmail.com>
2019-10-15 17:48:43 +0300

    adding reading configuration function

--- terraformize/terraformize_configure.py
+++ terraformize/terraformize_configure.py
@@ -1,14 +1,27 @@
 from parse_it import ParseIt
 
-# read config file at startup
-print("reading config variables")
-parser = ParseIt(config_location="config", recurse=True)
 
-print("reading config variables")
-basic_auth_user = parser.read_configuration_variable("basic_auth_user",  default_value=None)
-basic_auth_password = parser.read_configuration_variable("basic_auth_password",  default_value=None)
-auth_token = parser.read_configuration_variable("auth_token",  default_value=None)
-max_timeout = parser.read_configuration_variable("max_timeout",  default_value=600)
-terraform_binary_path = parser.read_configuration_variable("terraform_binary_path",  default_value=None)
-terraform_modules_path = parser.read_configuration_variable("terraform_modules_path",
-                                                            default_value="/www/terraform_modules")
+def read_configurations(config_folder: str = "config"):
+    """
+    Will create a config dict that includes all of the configurations for terraformize by aggregating from all valid
+    config sources (files, envvars, cli args, etc) & using sane defaults on config params that are not declared
+
+    Arguments:
+        :param config_folder: the folder which all configuration file will be read from recursively
+
+    Returns:
+        :return config: a dict of all configurations needed for terraformize to work
+    """
+    print("reading config variables")
+
+    config = {}
+    parser = ParseIt(config_location=config_folder, recurse=True)
+
+    config["basic_auth_user"] = parser.read_configuration_variable("basic_auth_user", default_value=None)
+    config["basic_auth_password"] = parser.read_configuration_variable("basic_auth_password", default_value=None)
+    config["auth_token"] = parser.read_configuration_variable("auth_token",  default_value=None)
+    config["max_timeout"] = parser.read_configuration_variable("max_timeout",  default_value=600)
+    config["terraform_binary_path"] = parser.read_configuration_variable("terraform_binary_path", default_value=None)
+    config["terraform_modules_path"] = parser.read_configuration_variable("terraform_modules_path",
+                                                                          default_value="/www/terraform_modules")
+    return config
--- test/test_terraformize_configure.py
+++ test/test_terraformize_configure.py
@@ -1,9 +1,42 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from terraformize.terraformize_configure import *
-import os
 
 
 class BaseTests(TestCase):
 
-    def test_file_reader_read_file(self):
-        pass
+    def test_terrformize_configure_sane_defaults(self):
+        reply = read_configurations()
+        expected_reply = {
+            'basic_auth_user': None,
+            'basic_auth_password': None,
+            'auth_token': None,
+            'max_timeout': 600,
+            'terraform_binary_path': None,
+            'terraform_modules_path': '/www/terraform_modules'
+        }
+        self.assertEqual(reply, expected_reply)
+
+    def test_terrformize_configure_read_envar(self):
+        with mock.patch('os.environ', {"MAX_TIMEOUT": "300", "AUTH_TOKEN": "my_super_secret_token"}):
+            reply = read_configurations()
+            expected_reply = {
+                'basic_auth_user': None,
+                'basic_auth_password': None,
+                'auth_token': "my_super_secret_token",
+                'max_timeout': 300,
+                'terraform_binary_path': None,
+                'terraform_modules_path': '/www/terraform_modules'
+            }
+            self.assertEqual(reply, expected_reply)
+
+    def test_terrformize_configure_read_custom_config_folder(self):
+        reply = read_configurations(config_folder="test_config")
+        expected_reply = {
+            'basic_auth_user': None,
+            'basic_auth_password': None,
+            'auth_token': "my_super_secret_token",
+            'max_timeout': 450,
+            'terraform_binary_path': None,
+            'terraform_modules_path': '/www/terraform_modules'
+        }
+        self.assertEqual(reply, expected_reply)
