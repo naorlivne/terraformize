29624139ded6f51d311e41322ab4b03dbfca9e5e
naorlivne <naorlivne@gmail.com>
2019-10-16 16:34:17 +0300

    commenting the endpoint codebase

--- terraformize/terraformize_endpoint.py
+++ terraformize/terraformize_endpoint.py
@@ -2,6 +2,7 @@ from terraformize.terraformize_terraform_warpper import *
 from terraformize.terraformize_configure import *
 from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
+from typing import Optional, Tuple
 import os
 
 
@@ -15,16 +16,35 @@ multi_auth = MultiAuth(basic_auth, token_auth)
 configuration = read_configurations(os.getenv("CONFIG_DIR", "config"))
 
 
-def terraform_return_code_to_http_code(terraform_return_code):
+def terraform_return_code_to_http_code(terraform_return_code: int) -> int:
+    """
+    Converts the exit code given by terraform to HTTP return code
+
+    Arguments:
+        :param terraform_return_code: the exit code given by terraform
+
+    Returns:
+        :return return_code: 200 if terraform exit code was 0, 400 otherwise
+    """
     if terraform_return_code == 0:
-        return 200
+        return_code = 200
     else:
-        return 400
+        return_code = 400
+    return return_code
 
 
-# this function checks basic_auth to allow access to authenticated users.
 @basic_auth.verify_password
-def verify_password(username, password):
+def verify_password(username: Optional[str], password: Optional[str]) -> bool:
+    """
+    Authenticate the basic_auth user & password given by the end-user
+
+    Arguments:
+        :param username: the username to check
+        :param password: the password to check
+
+    Returns:
+        :return: True if the user&pass is correct or auth is disabled, False otherwise
+    """
     # if auth_enabled is set to false then always allow access
     if configuration["auth_enabled"] is False:
         return True
@@ -38,7 +58,16 @@ def verify_password(username, password):
 
 # this function checks token based auth to allow access to authenticated users.
 @token_auth.verify_token
-def verify_token(token):
+def verify_token(token: Optional[str]) -> bool:
+    """
+    Authenticate the bearer token given by the end-user
+
+    Arguments:
+        :param token: the token to check
+
+    Returns:
+        :return: True if the token is correct or auth is disabled, False otherwise
+    """
     # if auth_enabled is set to false then always allow access
     if configuration["auth_enabled"] is False:
         return True
@@ -52,16 +81,36 @@ def verify_token(token):
 
 @multi_auth.login_required
 @app.route('/' + API_VERSION + '/<module_path>/<workspace_name>', methods=["POST"])
-def apply_terraform(module_path, workspace_name):
+def apply_terraform(module_path: str, workspace_name: str) -> Tuple[str, int]:
+    """
+    A rest endpoint to apply terraform modules at a given module path inside the main module directory path at a given
+    workspace
+
+    Arguments:
+        :param module_path:  the name of the subdirectory for the module inside the "terraform_modules_path" to run
+        "terraform apply" at
+        :param workspace_name: the name of the workspace to run "terraform apply" at
+
+    Returns:
+        :return return_body: a JSON of the stdout & stderr from the terraform run
+        :return terraform_return_code: the terraform return code
+
+    Exceptions:
+        :except FileNotFoundError: will return HTTP 404 with a JSON of the errorlog it catched from "terraform init" or
+        "terraform apply"
+        :except Exception: will return HTTP 400 with a JSON of the errorlog it catched from "terraform init" or
+        "terraform apply"
+    """
     try:
         terraform_object = Terraformize(workspace_name, configuration["terraform_modules_path"] + "/" + module_path,
                                         terraform_bin_path=configuration["terraform_binary_path"])
         terraform_return_code, terraform_stdout, terraform_stderr = terraform_object.apply(request.json)
-        return_body = {
+        return_body = jsonify({
             "stdout": terraform_stdout,
             "stderr": terraform_stderr
-        }
-        return jsonify(return_body), terraform_return_code_to_http_code(terraform_return_code)
+        })
+        terraform_return_code = terraform_return_code_to_http_code(int(terraform_return_code))
+        return return_body, terraform_return_code
     except FileNotFoundError as error_log:
         return jsonify({"error": str(error_log)}), 404
     except Exception as error_log:
@@ -70,16 +119,35 @@ def apply_terraform(module_path, workspace_name):
 
 @multi_auth.login_required
 @app.route('/' + API_VERSION + '/<module_path>/<workspace_name>', methods=["DELETE"])
-def destroy_terraform(module_path, workspace_name):
+def destroy_terraform(module_path: str, workspace_name: str) -> Tuple[str, int]:
+    """
+    A rest endpoint to destroy terraform modules at a given module path inside the main module directory path at a given
+    workspace
+
+    Arguments:
+        :param module_path:  the name of the subdirectory for the module inside the "terraform_modules_path" to run
+        "terraform destroy" at
+        :param workspace_name: the name of the workspace to run "terraform destroy" at
+
+    Returns:
+        :return return_body: a JSON of the stdout & stderr from the terraform run
+        :return terraform_return_code: the terraform return code
+
+    Exceptions:
+        :except FileNotFoundError: will return HTTP 404 with a JSON of the errorlog it catched from "terraform init" or
+        "terraform destroy"
+        :except Exception: will return HTTP 400 with a JSON of the errorlog it catched from "terraform init" or
+        "terraform destroy"
+    """
     try:
         terraform_object = Terraformize(workspace_name, configuration["terraform_modules_path"] + "/" + module_path,
                                         terraform_bin_path=configuration["terraform_binary_path"])
         terraform_return_code, terraform_stdout, terraform_stderr = terraform_object.destroy()
-        return_body = {
+        return_body = jsonify({
             "stdout": terraform_stdout,
             "stderr": terraform_stderr
-        }
-        return jsonify(return_body), terraform_return_code_to_http_code(terraform_return_code)
+        })
+        return return_body, terraform_return_code_to_http_code(int(terraform_return_code))
     except FileNotFoundError as error_log:
         return jsonify({"error": str(error_log)}), 404
     except Exception as error_log:
