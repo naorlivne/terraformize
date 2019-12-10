fbd374487c6e41022f1a6d9a1c4517e0fb7bcec1
naorlivne <naorlivne@gmail.com>
2019-12-10 12:00:40 +0200

    dont really need double tap of init, just one init before creating the workspace seems enough

--- terraformize/terraformize_terraform_wrapper.py
+++ terraformize/terraformize_terraform_wrapper.py
@@ -18,16 +18,13 @@ class Terraformize:
         self.tf = Terraform(working_dir=folder_path, terraform_bin_path=self.terraform_bin_path)
 
         # we need to init for a remote backend before we can create a workspace
-        self.tf.init(dir_or_plan=folder_path)
+        self.init_return_code, self.init_stdout, self.init_stderr = self.tf.init(dir_or_plan=folder_path)
 
         # always create the workspace and switch to it, if workspace already created carries on when it failed creating
         # it again
         self.tf.create_workspace(workspace=workspace)
         self.workspace_return_code, self.workspace_stdout, self.workspace_stderr = \
             self.tf.set_workspace(workspace=workspace)
-
-        # always init the directory
-        self.init_return_code, self.init_stdout, self.init_stderr = self.tf.init(dir_or_plan=folder_path)
 
     def apply(self, variables: Optional[dict] = None, parallelism: int = 10) -> Tuple[str, str, str]:
         """
