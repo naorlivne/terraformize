5d02287bdd4ccc320f25a5aac87cc83cdf3edbd4
Chase Bolt <chase.bolt@gmail.com>
2019-12-11 21:47:44 -0800

    template gunicorn config for docker

--- config/config.py
+++ config/config.py
@@ -1,9 +1,17 @@
-bind = "0.0.0.0:80"
-chdir = "/www"
-loglevel = "error"
-workers = "1"
-worker_class = "sync"
-threads = "1"
+import os
 
+
+host = os.getenv("HOST") if os.getenv("HOST") else "0.0.0.0"
+port = os.getenv("PORT") if os.getenv("PORT") else "80"
+bind = f"{host}:{port}"
+
+worker_class = os.getenv("WORKER_CLASS") if os.getenv("WORKER_CLASS") else "sync"
+workers = os.getenv("WORKERS") if os.getenv("WORKERS") else "1"
+threads = os.getenv("THREADS") if os.getenv("THREADS") else "1"
+preload = os.getenv("PRELOAD") if os.getenv("PRELOAD") else False
+
+chdir = "/www"
+
+loglevel = os.getenv("LOG_LEVEL") if os.getenv("LOG_LEVEL") else "error"
 errorlog = "-"
 accesslog = "-"
