2f2fab6f6ac99482b2652fef02842e6567a3dca9
Naor Livne <naorlivne@gmail.com>
2021-05-01 10:57:53 +0300

    adding curl tests body on error

--- test/end2end_tests.sh
+++ test/end2end_tests.sh
@@ -11,17 +11,17 @@
 set -e
 
 # wait until the endpoint becomes active
-until $(curl --output /dev/null --silent --head --fail -H 'cache-control: no-cache' http://run_end2end_tests_terraformize_service/v1/health); do
+until $(curl --output /dev/null --silent --head --fail-with-body -H 'cache-control: no-cache' http://run_end2end_tests_terraformize_service/v1/health); do
     echo "Waiting on Terraformize API to become available..."
     sleep 1
 done
 
 # checking health endpoint
-curl -f -X GET http://run_end2end_tests_terraformize_service/v1/health
+curl --fail-with-body -X GET http://run_end2end_tests_terraformize_service/v1/health
 
 
 # checking a terraform apply
-curl -f -X POST \
+curl --fail-with-body -X POST \
   http://run_end2end_tests_terraformize_service/v1/working_test_remote_backend/my_workspace \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
@@ -30,7 +30,7 @@ curl -f -X POST \
 }'
 
 # checking a terraform apply on a 2nd workspace
-curl -f -X POST \
+curl --fail-with-body -X POST \
   http://run_end2end_tests_terraformize_service/v1/working_test_remote_backend/my_other_workspace \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
@@ -39,7 +39,7 @@ curl -f -X POST \
 }'
 
 # checking terraform destroy
-curl -f -X DELETE \
+curl --fail-with-body -X DELETE \
   http://run_end2end_tests_terraformize_service/v1/working_test_remote_backend/my_workspace \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
@@ -48,7 +48,7 @@ curl -f -X DELETE \
 }'
 
 # checking terraform destroy on a 2nd workspace
-curl -f -X DELETE \
+curl --fail-with-body -X DELETE \
   http://run_end2end_tests_terraformize_service/v1/working_test_remote_backend/my_other_workspace \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
