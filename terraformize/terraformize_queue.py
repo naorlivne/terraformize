d5b08b525c983e47e52bb599e65c3d3d86571a3c
naorlivne <naorlivne@gmail.com>
2022-11-10 14:03:45 +0200

    WIP prefood push for work needed to add queue functionality

--- terraformize/terraformize_queue.py
+++ terraformize/terraformize_queue.py
@@ -1,6 +1,21 @@
 import pika
 import os
 import sys
+import json
+
+
+def extract_params_from_queue_json(message_json: bytes) -> dict:
+    """
+    Checks if auth is enabled by making sure if there is a username & password pair or a token configured
+
+    Arguments:
+        :param message_json: the json message as read from the rabbit queue to filter down to params
+
+    Returns:
+        :return decoded_dict: the name of the folder where the module is used
+    """
+    decoded_dict = json.loads(message_json.decode())
+    return decoded_dict
 
 
 class RabbitWorker:
@@ -8,13 +23,15 @@ class RabbitWorker:
     def __init__(self, rabbit_url_connection_string: str, read_queue: str = "terraformize_read_queue",
                  reply_queue: str = "terraformize_reply_queue"):
         """
-        Will create a rabbit connection & declare it's queues
+        Checks if auth is enabled by making sure if there is a username & password pair or a token configured
 
         Arguments:
-            :param rabbit_url_connection_string: the URL connection string to RabbitMQ, for more info look at
-            https://pika.readthedocs.io/en/stable/modules/parameters.html#urlparameters
-            :param read_queue: the queue to read from
-            :param reply_queue: the queue to respond with the results of the run to
+            :param username: the possible username for authentication
+            :param password: the possible password for authentication
+            :param token: the possible token for authentication
+
+        Returns:
+            :return auth_required: True if auth is enabled, False otherwise
         """
         self.read_queue = read_queue
         self.reply_queue = reply_queue
@@ -25,7 +42,7 @@ class RabbitWorker:
         self.publish_channel = self.publish_connection.channel()
         self.publish_channel.queue_declare(queue=reply_queue, durable=True)
 
-    def callback(self, ch, method, properties, body: str):
+    def callback(self, ch, method, properties, body: bytes):
         """
         the callback that actually reads & process a message read from the queue then publishes the run result to the
         reply_queue
@@ -34,7 +51,10 @@ class RabbitWorker:
             :param ch: the channel the callback is using, taken internally from pika
             :param method: the method the callback is using, taken internally from pika
             :param properties: the queue properties, taken internally from pika
-            :param body: the json body in string form of the message read from the the queue
+            :param body: the json body in bytes form of the message read from the the queue
+
+        Exceptions:
+            :except Exception: will print the error and exit the container with exit code 2
         """
         try:
             # TODO actual code here that reads the 'body' of the message and runs the proper terraformize command then
@@ -48,6 +68,9 @@ class RabbitWorker:
     def read_from_queue(self):
         """
         Will start consuming messages from a queue
+
+        Exceptions:
+            :except Exception: will print the error and exit the container with exit code 2
         """
         try:
             self.consume_channel.basic_qos(prefetch_count=1)
@@ -67,7 +90,7 @@ class RabbitWorker:
         self.publish_channel.basic_publish(
             exchange='',
             routing_key=self.reply_queue,
-            body=message,
+            body=bytes(json.dumps(message)),
             properties=pika.BasicProperties(
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             )
